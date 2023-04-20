c0676e578a932681008f672f66c7e8bd673278ac
shang906 <jiashang@ucdavis.edu>
2023-04-19 22:59:49 -0700

    video

---
+++ studio1/script.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+    const fs = document.querySelector('i');
+    const mySection = document.querySelector('#mySection');
+    const line1 = document.querySelector('#line1');
+    const mountain = document.querySelector('#mountain');
+    const water = document.querySelector('#water');
+    const joy = document.querySelector('#joy');
+
+    // const intervalID = setInterval(checkTime, 1000);
+
+    fs.addEventListener('click', function(){
+        if (!document.fullscreenElement){
+            document.documentElement.requestFullscreen();
+        } else{
+            document.exitFullscreen();
+        }
+    })
+
+    window.addEventListener('load', function () {
+
+		const pre = document.getElementById('pre');
+		pre.className = 'hidden';
+
+		pre.addEventListener('animationend', function () {
+			pre.style.display = "none";
+		});
+    })
+
+    let counter = 0;
+    const line = ['line1', 'water', 'mountain', 'joy'];
+    function fadeInLine(counter){
+        document.querySelector(`#${line[counter]}`).className = 'appear';
+        setTimeout(function(){
+            document.querySelector(`#${line[counter]}`).className = 'hidden';
+            if(counter < line.length-1){
+                counter++;
+            } else {
+                counter = 0;
+            }
+            fadeInLine(counter);
+        }, 3000);
+    }
+
+    fadeInLine(counter);
+})()
+
